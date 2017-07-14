7614ce922f4db481bd588f8dabb7bc322142aaa8
Malte Bruweleit <malte.bruweleit@mspag.com>
2017-07-14 15:59:16 +0200

    changed position of explosion source of boxes

--- src/app/game/model/ColoredShape.ts
+++ src/app/game/model/ColoredShape.ts
@@ -29,28 +29,28 @@ export class ColoredShape extends Shape {
 
   onHit(game: Game): any {
     if(this.destructible) {
-      let fragment = new ColoredShape(this.x, this.y, this.z, this.h / 2, this.w / 2, this.color, false);
+      let fragment = new ColoredShape(this.x + this.h / 4, this.y + this.w / 4, this.z, this.h / 2, this.w / 2, this.color, false);
       fragment.addBehaviour("drift-off", (game, shape) => {
         shape.move(-1, -1);
         if((game.gameTime - shape.birth) > 35) {
           game.gameArea.removeElement(shape);
         }
       });
-      let fragment2 = new ColoredShape(this.x, this.y, this.z, this.h / 2, this.w / 2, this.color, false);
+      let fragment2 = new ColoredShape(this.x + this.h / 4, this.y + this.w / 4, this.z, this.h / 2, this.w / 2, this.color, false);
       fragment2.addBehaviour("drift-off", (game, shape) => {
         shape.move(1, -1);
         if((game.gameTime - shape.birth) > 35) {
           game.gameArea.removeElement(shape);
         }
       });
-      let fragment3 = new ColoredShape(this.x, this.y, this.z, this.h / 2, this.w / 2, this.color, false);
+      let fragment3 = new ColoredShape(this.x + this.h / 4, this.y + this.w / 4, this.z, this.h / 2, this.w / 2, this.color, false);
       fragment3.addBehaviour("drift-off", (game, shape) => {
         shape.move(1, 1);
         if((game.gameTime - shape.birth) > 35) {
           game.gameArea.removeElement(shape);
         }
       });
-      let fragment4 = new ColoredShape(this.x, this.y, this.z, this.h / 2, this.w / 2, this.color, false);
+      let fragment4 = new ColoredShape(this.x + this.h / 4, this.y + this.w / 4, this.z, this.h / 2, this.w / 2, this.color, false);
       fragment4.addBehaviour("drift-off", (game, shape) => {
         shape.move(-1, 1);
         if((game.gameTime - shape.birth) > 35) {
