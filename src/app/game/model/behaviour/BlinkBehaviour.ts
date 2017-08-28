9e9f25ab971a8b0bb8516f2b6cf97207992f7590
Malte Bruweleit <malte.bruweleit@mspag.com>
2017-08-28 18:34:26 +0200

    added blink behaviour with parameters for intervals

---
+++ src/app/game/model/behaviour/BlinkBehaviour.ts
@@ -0,0 +1,35 @@
+import {Behaviour} from "./Behaviour";
+import {Game} from "../../service/Game";
+import {Position} from "../base/Position";
+import {Element} from "../base/Element";
+
+export class BlinkBehaviour implements Behaviour<Element> {
+  private tempPosition: Position;
+  private blink: boolean = false;
+  private invisibleInterval: number;
+  private visibleInterval: number;
+  private lastBlink: number = 0;
+
+  constructor(element: Element, gameTime: number, visibleInterval: number, invisibleInterval: number) {
+    this.tempPosition = new Position(element.x, element.y);
+    this.visibleInterval = visibleInterval;
+    this.invisibleInterval = invisibleInterval;
+    this.lastBlink = gameTime;
+  }
+
+  behaviour: (game: Game, shape: Element) => void = (game, element) => {
+    if(this.blink) {
+      element.setPosition(-5000, element.y);
+      if(game.gameTime - this.lastBlink >= this.invisibleInterval) {
+        this.lastBlink = game.gameTime;
+        this.blink = false;
+      }
+    } else {
+      element.setPosition(this.tempPosition.x, element.y);
+      if(game.gameTime - this.lastBlink >= this.visibleInterval) {
+        this.lastBlink = game.gameTime;
+        this.blink = true
+      }
+    }
+  }
+}
