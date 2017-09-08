c874ddbbef38f6d43eae60802b7b25849336a520
Malte Bruweleit <malte.bruweleit@mspag.com>
2017-09-09 01:31:35 +0200

    changed circle behaviour to be more configurable

--- src/app/game/model/behaviour/CircleBehaviour.ts
+++ src/app/game/model/behaviour/CircleBehaviour.ts
@@ -4,17 +4,23 @@ import {Shape} from "../base/Shape";
 
 export class CircleBehaviour implements Behaviour<Shape> {
   radius: number;
-  angle: number;
+  speed: number;
+  private angle: number;
 
-  constructor(radius: number) {
+  constructor(radius: number, speed: number, shape: Shape) {
     this.radius = radius;
-    this.angle = Math.PI / 180;
+    this.speed = speed;
+    this.angle = Math.PI / 360 * this.speed;
+    this.centerX = shape.position.x;
+    this.centerY = shape.position.y;
   }
 
+  private centerX: number;
+  private centerY: number;
   behaviour: (game: Game, shape: Shape) => void = (game, shape) => {
-    this.angle += Math.PI / 180;
-    let x = 500 + this.radius * (Math.cos(this.angle));
-    let y = 250 + this.radius * (Math.sin(this.angle));
+    this.angle += Math.PI / 360 * this.speed;
+    let x = this.centerX + this.radius * (Math.cos(this.angle));
+    let y = this.centerY + this.radius * (Math.sin(this.angle));
     shape.setPosition(x, y);
   }
 }
