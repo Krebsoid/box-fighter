fd6ba63d7f6e7a3bed04e942a0ef6cd10c6a6d20
Malte Bruweleit <malte.bruweleit@mspag.com>
2017-09-19 19:49:21 +0200

    Fuelmeter now checks the capacity of players engine

--- src/app/game/model/player/ui/FuelMeter.ts
+++ src/app/game/model/player/ui/FuelMeter.ts
@@ -9,13 +9,17 @@ export class FuelMeter extends Element {
   elements: Element[] = [];
   player: Player;
   filling: ColoredShape;
+  outerBorder: ColoredShape;
+  innerBorder: ColoredShape;
   fixed: boolean = true;
 
   constructor(player: Player) {
     super(300, 450, 10000);
     this.player = player;
-    this.elements.push(new ColoredShape(this.position.x, this.position.y, this.z, 30, player.engine.capacity + 6, "#000").isFixed(true));
-    this.elements.push(new ColoredShape(this.position.x + 3, this.position.y + 3, this.z + 1, 24, player.engine.capacity, "#FFF").isFixed(true));
+    this.outerBorder = new ColoredShape(this.position.x, this.position.y, this.z, 30, player.engine.capacity + 6, "#000").isFixed(true);
+    this.innerBorder = new ColoredShape(this.position.x + 3, this.position.y + 3, this.z + 1, 24, player.engine.capacity, "#FFF").isFixed(true);
+    this.elements.push(this.outerBorder);
+    this.elements.push(this.innerBorder);
     this.filling = new ColoredShape(this.position.x + 3, this.position.y + 3, this.position.x + 2, 24, player.engine.level, "#FF0000").isFixed(true);
     this.elements.push(this.filling);
   }
@@ -33,6 +37,8 @@ export class FuelMeter extends Element {
 
   update(game: Game) {
     if(this.player.engine) {
+      this.innerBorder.w = this.player.engine.capacity;
+      this.outerBorder.w = this.player.engine.capacity + 6;
       if(this.player.engine.level > this.filling.w) {
         this.filling.w += this.updateMapping(Math.abs(this.player.engine.level - this.filling.w));
       }
